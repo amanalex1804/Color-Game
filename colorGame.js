e55d7d5648d7a02ae11db1c9f231e23d8d8705a7
amanalex1804 <kumarrocky436@gmail.com>
2019-05-31 12:39:42 +0530

    Second Part

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,42 @@ var colors =[
 ]
 
 var squares  = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
 	squares[i].style.backgroundColor = colors[i];
+    
+    //click listeners
+    
+    squares[i].addEventListener("click",function(){
+    	// grab color of clicked square
+    	var clickedColor =  this.style.backgroundColor;
+
+    	// compare
+    	if(clickedColor === pickedColor){
+    		messageDisplay.textContent = "Correct";
+    		changeColors(clickedColor);
+    		
+    	}else{
+    		this.style.backgroundColor = "#232323";
+    		messageDisplay.textContent = "Try Again";
+    	}
+    });
+
+}
+
+function changeColors(color){
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random()*colors.length);
+	return colors[random];
 }
